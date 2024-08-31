4b14dc0a5d24d62c9a1239a83ff0f48ee3922a97
JerryWu <plefyion@gmail.com>
2024-08-31 18:16:54 +0800

    semaphore practice

--- JerryKit/Practice/GCP/GCPSemaphorePractice.swift
+++ JerryKit/Practice/GCP/GCPSemaphorePractice.swift
@@ -8,32 +8,74 @@
 
 class GCPSemaphorePractice {
     func startTest() {
-        test1()
+//        test1()
+        test2()
     }
 }
 extension GCPSemaphorePractice {
 //    test start
 //    test wait
-//    等3秒
 //    process start
+//    等3秒
 //    process end
 //    test end
     private func test1() {
-        semaphoreTest()
-    }
-    private func semaphoreTest() {
         print("test start")
         let semaphore = DispatchSemaphore(value: 0)
-
         DispatchQueue.global().async {
             print("process start")
             sleep(3)
             print("process end")
             semaphore.signal() // 任務完成
         }
-
         print("test wait")
         semaphore.wait() // 等待任務完成後，更新UI
         print("test end")
     }
+    private func test2() {
+        GCPSemaphorePractice2().startCount()
+    }
+}
+class GCPSemaphorePractice2 {
+    let semaphore = DispatchSemaphore(value: 1)
+    
+    // Shared resource
+    var resourceCounter = 0
+    
+    func startCount() {
+        
+        // Create a concurrent queue
+        let concurrentQueue = DispatchQueue(label: "Queue", attributes: .concurrent)
+        
+        // Run the tasks concurrently
+        concurrentQueue.async { [self] in
+            taskOne()
+        }
+        
+        concurrentQueue.async { [self] in
+            taskTwo()
+        }
+    }
+    
+    // Concurrent tasks
+    private func taskOne() {
+        semaphore.wait() // Acquire the semaphore permit
+        for _ in 1...5 {
+            print(Thread.current)
+            resourceCounter += 1
+            print("Task One: \(resourceCounter)")
+        }
+        semaphore.signal() // Release the semaphore permit
+    }
+    
+    private func taskTwo() {
+        semaphore.wait() // Acquire the semaphore permit
+        for _ in 1...5 {
+            print(Thread.current)
+            resourceCounter -= 1
+            print("Task Two: \(resourceCounter)")
+        }
+        semaphore.signal() // Release the semaphore permit
+    }
+   
 }
