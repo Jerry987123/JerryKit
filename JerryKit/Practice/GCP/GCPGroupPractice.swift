bec0c585d3dae44f9fee9318f8d82e34b429a5d0
JerryWu <plefyion@gmail.com>
2024-08-31 15:19:16 +0800

    add gcp group example

--- JerryKit/Practice/GCP/GCPGroupPractice.swift
+++ JerryKit/Practice/GCP/GCPGroupPractice.swift
@@ -7,13 +7,15 @@
 //
 class GCPGroupPractice {
     func startTest() {
-        startTest1()
+//        startTest1()
+        startTest2()
     }
 }
 extension GCPGroupPractice {
 //    queue1: 1
 //    queue1: 2
 //    queue1: 3
+//    等3秒
 //    queue2: 1
 //    queue2: 2
 //    queue2: 3
@@ -38,6 +40,39 @@ extension GCPGroupPractice {
         }
           
         group.wait()
+        print("wait a minute")
+        group.notify(queue: .main) {
+          print("tasks finished")
+        }
+    }
+//    wait a minute
+//    queue1: 1
+//    queue1: 2
+//    queue1: 3
+//    等3秒
+//    queue2: 1
+//    queue2: 2
+//    queue2: 3
+//    tasks finished
+    private func startTest2() {
+        let group = DispatchGroup()
+                
+        let queue1 = DispatchQueue(label: "GCPGroup1", attributes: .concurrent)
+        queue1.async(group: group) {
+          for i in 1...3 {
+            print("queue1: \(i)")
+          }
+        }
+                
+        let queue2 = DispatchQueue(label: "GCPGroup2", attributes: .concurrent)
+        queue2.async(group: group) {
+          sleep(3)
+          for i in 1...3 {
+            print("queue2: \(i)")
+          }
+        }
+          
+//        group.wait()
         print("wait a minute")
         group.notify(queue: .main) {
           print("tasks finished")
