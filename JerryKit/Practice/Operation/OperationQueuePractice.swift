5f25877f9054d9c28b2dc0778913e0167dee735c
JerryWu <plefyion@gmail.com>
2024-09-01 11:20:24 +0800

    OperationQueue addDependency

--- JerryKit/Practice/Operation/OperationQueuePractice.swift
+++ JerryKit/Practice/Operation/OperationQueuePractice.swift
@@ -8,7 +8,8 @@
 
 class OperationQueuePractice: BasePractice {
     func startTest() {
-        test()
+//        test()
+        test2()
     }
 }
 extension OperationQueuePractice {
@@ -24,5 +25,36 @@ extension OperationQueuePractice {
             
         //阻塞在这里
         printerQueue.waitUntilAllOperationsAreFinished()
+    }
+    
+    private func test2() {
+        let queue = OperationQueue()
+
+        var flag = false
+        let operation1 = BlockOperation {
+            // 模拟一个操作是否成功
+            flag = true
+            print("Operation 1 in \(Thread.current).")
+            Thread.sleep(forTimeInterval: 2)
+        }
+
+        // 监听 Operation 1 是否完成
+        operation1.completionBlock = {
+            print("Operation 1 is completed.")
+        }
+
+        let operation2 = BlockOperation {
+            if flag {
+                print("Operation 2 in \(Thread.current).")
+            } else {
+                print("Something went wrong.")
+            }
+        }
+
+        operation2.addDependency(operation1)
+
+        // 过两秒之后控制台才会打印 Operation1 完成和 Operation2 的执行信息
+        queue.addOperation(operation1)
+        queue.addOperation(operation2)
     }
 }
